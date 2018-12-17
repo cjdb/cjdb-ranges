4f56d5464d819590082ab1578a0d05834f8a088e
Christopher Di Bella <cjdb.ns@gmail.com>
2018-12-17 09:50:15 +0000

    Created [range.cmp] and extension points [skip ci]

---
+++ include/cjdb/detail/functional/rangecmp.hpp
@@ -0,0 +1,26 @@
+//
+//  Copyright Christopher Di Bella
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef CJDB_DETAIL_FUNCTIONAL_RANGECMP_HPP
+#define CJDB_DETAIL_FUNCTIONAL_RANGECMP_HPP
+
+#include "cjdb/detail/functional/rangecmp/equal_to.hpp"
+#include "cjdb/detail/functional/rangecmp/not_equal_to.hpp"
+#include "cjdb/detail/functional/rangecmp/less.hpp"
+#include "cjdb/detail/functional/rangecmp/less_equal.hpp"
+#include "cjdb/detail/functional/rangecmp/greater.hpp"
+#include "cjdb/detail/functional/rangecmp/greater_equal.hpp"
+
+#endif // CJDB_DETAIL_FUNCTIONAL_RANGECMP_HPP
---
+++ include/cjdb/detail/functional/rangecmp/less_equal.hpp
@@ -0,0 +1,46 @@
+//
+//  Copyright Christopher Di Bella
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef CJDB_DETAIL_FUNCTIONAL_RANGECMP_LESS_EQUAL_HPP
+#define CJDB_DETAIL_FUNCTIONAL_RANGECMP_LESS_EQUAL_HPP
+
+#include "cjdb/detail/concepts/comparison/strictotallyordered.hpp"
+#include "cjdb/detail/functional/rangecmp/less.hpp"
+#include "cjdb/detail/type_traits/type_traits.hpp"
+#include <utility>
+
+namespace cjdb::ranges {
+   /// \brief Function object for performing comparisons. Applies operator<=.
+   ///
+   struct less_equal {
+      /// \returns `true` if `t` is not greater than `u`, `false` otherwise.
+      ///
+      template<class T, StrictTotallyOrderedWith<T> U>
+      constexpr bool operator()(T&& t, U&& u) const
+      CJDB_NOEXCEPT_RETURN(
+         not greater{}(std::forward<T>(t), std::forward<U>(u))
+      )
+
+      using is_transparent = true_type;
+   };
+
+   /// \brief
+   /// \note This is an extension.
+   ///
+   template<StrictTotallyOrdered T>
+   using less_equal_unary = detail_inequality_unary::inequality_unary<T, less_equal>{};
+} // namespace cjdb::ranges
+
+#endif // CJDB_DETAIL_FUNCTIONAL_RANGECMP_LESS_EQUAL_HPP
