15f19ac6046cfc8b58af8079e5cd9e4b8ede916e
Christopher Di Bella <cjdb.ns@gmail.com>
2019-10-09 22:35:07 +1100

    adds ITER_TRAITS and ITER_CONCEPT

---
+++ include/cjdb/detail/iterator/iter_traits.hpp
@@ -0,0 +1,56 @@
+// Copyright (c) Christopher Di Bella.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+#ifndef CJDB_DETAIL_ITERATOR_ITER_TRAITS_HPP
+#define CJDB_DETAIL_ITERATOR_ITER_TRAITS_HPP
+
+#include "cjdb/type_traits/is_primary.hpp"
+#include "cjdb/iterator/iterator_traits.hpp"
+
+namespace cjdb::ranges::detail_iter_traits {
+	template<typename I>
+	struct iter_traits : iterator_traits<I> {};
+
+	template<typename I>
+	requires is_primary<iterator_traits<I>>
+	struct iter_traits<I> : I {};
+
+	template<typename I>
+	struct iter_traits<I* volatile> {};
+
+	template<typename I>
+	concept has_iterator_concept_member =
+		requires { typename iter_traits<I>::iterator_concept; };
+
+	template<typename I>
+	concept has_iterator_category_member =
+		not has_iterator_concept_member<I> and
+		requires { typename iter_traits<I>::iterator_category; };
+
+	template<typename I>
+	concept iter_concept_fallback =
+		is_primary<iterator_traits<I>> and not has_iterator_category_member<I>;
+
+	template<typename I>
+	struct iter_concept {};
+
+	template<has_iterator_concept_member I>
+	struct iter_concept<I> {
+		using type = typename iter_traits<I>::iterator_concept;
+	};
+
+	template<has_iterator_category_member I>
+	struct iter_concept<I> {
+		using type = typename iter_traits<I>::iterator_category;
+	};
+
+	template<iter_concept_fallback I>
+	struct iter_concept<I> {
+		using type = std::random_access_iterator_tag;
+	};
+
+	template<typename I>
+	using iter_concept_t = _t<iter_concept<I>>;
+} // namespace cjdb::ranges::detail_iter_traits
+
+#endif // CJDB_DETAIL_ITERATOR_ITER_TRAITS_HPP
---
+++ test/unit/iterator/iter_traits.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) Christopher Di Bella.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+#include "cjdb/detail/iterator/iter_traits.hpp"
+
+#include "cjdb/iterator/incrementable_traits.hpp"
+#include "cjdb/iterator/readable_traits.hpp"
+#include "cjdb/concepts/core/same_as.hpp"
+#include "cjdb/iterator/iterator_traits.hpp"
+#include <iterator>
+
+struct random_access_iter_concept {
+	using value_type = int;
+	using difference_type = int;
+
+	int operator*() const;
+	random_access_iter_concept& operator++();
+	random_access_iter_concept operator++(int);
+	random_access_iter_concept& operator--();
+	random_access_iter_concept operator--(int);
+
+	bool operator==(random_access_iter_concept) const;
+	bool operator!=(random_access_iter_concept) const;
+};
+
+using cjdb::same_as;
+
+static_assert(same_as<cjdb::iterator_traits<random_access_iter_concept>::iterator_category,
+                      std::input_iterator_tag>);
+static_assert(same_as<cjdb::ranges::detail_iter_traits::iter_concept_t<random_access_iter_concept>,
+                      std::random_access_iterator_tag>);
+
+struct bidirectional_iter_concept {
+	using iterator_category = std::bidirectional_iterator_tag;
+	using value_type = int;
+	using difference_type = int;
+
+	int operator*() const;
+	bidirectional_iter_concept& operator++();
+	bidirectional_iter_concept operator++(int);
+	bidirectional_iter_concept& operator--();
+	bidirectional_iter_concept operator--(int);
+
+	bool operator==(bidirectional_iter_concept) const;
+	bool operator!=(bidirectional_iter_concept) const;
+};
+
+static_assert(same_as<cjdb::iterator_traits<bidirectional_iter_concept>::iterator_category,
+                      std::bidirectional_iterator_tag>);
+static_assert(same_as<cjdb::ranges::detail_iter_traits::iter_concept_t<bidirectional_iter_concept>,
+                      std::bidirectional_iterator_tag>);
+
+static_assert(same_as<cjdb::ranges::detail_iter_traits::iter_concept_t<int*>,
+                      cjdb::contiguous_iterator_tag>);
+static_assert(same_as<cjdb::ranges::detail_iter_traits::iter_concept_t<int* volatile>,
+                      std::random_access_iterator_tag>);
+
+int main() {}
