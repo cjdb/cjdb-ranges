174db4fe7f090d96339986f66f309429d3f8f185
Christopher Di Bella <cjdb.ns@gmail.com>
2019-10-20 10:50:45 +1100

    starts work for indirectly_movable[_storable]

---
+++ include/cjdb/iterator/concepts/algorithm_requirements/indirectly_movable.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) Christopher Di Bella.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+#ifndef CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_HPP
+#define CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_HPP
+
+#include "cjdb/detail/iterator/iter_move.hpp"
+#include "cjdb/iterator/concepts/readable.hpp"
+#include "cjdb/iterator/concepts/writable.hpp"
+
+namespace cjdb {
+	/// \brief The `indirectly_­movable` concept specifies the relationship between a `readable` type
+	///        and a `writable` type between which values may be moved.
+	///
+	template<typename In, typename Out>
+	concept indirectly_movable = readable<In> and writable<Out, iter_rvalue_reference_t<In>>;
+} // namespace cjdb
+
+#endif // CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_HPP
---
+++ include/cjdb/iterator/concepts/algorithm_requirements/indirectly_movable_storable.hpp
@@ -0,0 +1,40 @@
+// Copyright (c) Christopher Di Bella.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+#ifndef CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_STORABLE_HPP
+#define CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_STORABLE_HPP
+
+#include "cjdb/concepts/core/assignable_from.hpp"
+#include "cjdb/concepts/core/constructible_from.hpp"
+#include "cjdb/concepts/object/movable.hpp"
+#include "cjdb/detail/iterator/iter_move.hpp"
+#include "cjdb/detail/iterator/readable_traits.hpp"
+#include "cjdb/iterator/concepts/writable.hpp"
+#include "cjdb/iterator/concepts/indirectly_movable.hpp"
+
+namespace cjdb {
+	/// \brief The `indirectly_­movable_­storable` concept augments `indirectly_­movable` with
+	///        additional requirements enabling the transfer to be performed through an intermediate
+	///        object of the `readable` type's value type.
+	///
+	/// \pre **Axiom:**
+	/// Let `i` be a dereferenceable value of type `In`. `In` and `Out` model
+	/// `indirectly_­movable_­storable<In, Out>` only if after the initialization of the object `obj`
+	/// in
+	/// ```cpp
+	/// iter_value_t<In> obj(ranges::iter_move(i));
+	/// ```
+	/// `obj` is equal to the value previously denoted by `*i`. If `iter_­rvalue_­reference_­t<In>` is
+	/// an rvalue reference type, the resulting state of the value denoted by `*i` is valid but
+	/// unspecified ([lib.types.movedfrom]).
+	///
+	template<typename In, typename Out>
+	concept indirectly_movable_storable =
+		indirectly_movable<In, Out> and
+		writable<Out, iter_value_t<In>> and
+		movable<iter_value_t<In>> and
+		constructible_from<iter_value_t<In>, iter_rvalue_reference_t<In>> and
+		assignable_from<iter_value_t<In>&, iter_rvalue_reference_t<In>>;
+} // namespace cjdb
+
+#endif // CJDB_ITERATOR_CONCEPTS_ALGORITHM_INDIRECTLY_MOVABLE_STORABLE_HPP
